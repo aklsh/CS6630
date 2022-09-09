e1d5d6d71fbd496b58ac4e650f88abcd3fd64c70
Akilesh Kannan <aklsh@tuta.io>
2022-09-09 15:47:39 +0530

    feat: found common key among all ct-fct pairs

--- A3/dfa_8.py
+++ A3/dfa_8.py
@@ -1,4 +1,5 @@
 import numpy as np
+from multiprocessing import Pool
 import pandas as pd
 
 ct = np.load("ciphertexts.npy")
@@ -16,68 +17,128 @@ def sbox(byte):
 def sbox_inv(byte):
     return s_[byte]
 
-def solve(correct, faulty, bytes, factors):
-    assert(len(bytes) == 4)
-    assert(len(factors) == 4)
-    assert(len(correct) == 16)
-    assert(len(faulty) == 16)
+def get_fault_column(position):
+    if position == 0 or position == 5 or position == 10 or position == 15:
+        return 0
+    elif position == 4 or position == 9 or position == 14 or position == 3:
+        return 1
+    elif position == 8 or position == 13 or position == 2 or position == 7:
+        return 2
+    elif position == 12 or position == 1 or position == 6 or position == 11:
+        return 3
+    else:
+        return None
 
+def get_factors(column):
+    if column == 0:
+        return [[2, 1, 1, 3],
+                [1, 1, 3, 2],
+                [1, 3, 2, 1],
+                [3, 2, 1, 1]]
+    elif column == 1:
+        return [[3, 2, 1, 1],
+                [2, 1, 1, 3],
+                [1, 1, 3, 2],
+                [1, 3, 2, 1]]
+    elif column == 2:
+        return [[1, 3, 2, 1],
+                [3, 2, 1, 1],
+                [2, 1, 1, 3],
+                [1, 1, 3, 2]]
+    elif column == 3:
+        return [[1, 1, 3, 2],
+                [1, 3, 2, 1],
+                [3, 2, 1, 1],
+                [2, 1, 1, 3]]
+    else:
+        return None
+
+# Solve 1 set of equations (4 key bytes)
+def solve(correct, faulty, byte_positions, factors):
     possibilities = []
     for delta in range(256):
-        b0_sols = []
-        b1_sols = []
-        b2_sols = []
-        b3_sols = []
+        b_sols = []
 
-        # eqn 1
-        lhs = mult[factors[0],delta]
-        for b0 in range(256):
-            rhs = sbox_inv(correct[bytes[0]] ^ b0) ^ sbox_inv(faulty[bytes[0]] ^ b0)
-            if lhs == rhs:
-                b0_sols.append(b0)
-        # eqn 2
-        lhs = mult[factors[1],delta]
-        for b1 in range(256):
-            rhs = sbox_inv(correct[bytes[1]] ^ b1) ^ sbox_inv(faulty[bytes[1]] ^ b1)
-            if lhs == rhs:
-                b1_sols.append(b1)
-        # eqn 3
-        lhs = mult[factors[2],delta]
-        for b2 in range(256):
-            rhs = sbox_inv(correct[bytes[2]] ^ b2) ^ sbox_inv(faulty[bytes[2]] ^ b2)
-            if lhs == rhs:
-                b2_sols.append(b2)
-        # eqn 4
-        lhs = mult[factors[3],delta]
-        for b3 in range(256):
-            rhs = sbox_inv(correct[bytes[3]] ^ b3) ^ sbox_inv(faulty[bytes[3]] ^ b3)
-            if lhs == rhs:
-                b3_sols.append(b3)
+        for i in range(4):
+            bi_sols = []
+            lhs = mult[factors[i],delta]
+            for kb in range(256):
+                rhs = sbox_inv(correct[byte_positions[i]] ^ kb) ^ sbox_inv(faulty[byte_positions[i]] ^ kb)
+                if lhs == rhs:
+                    bi_sols.append(kb)
+            b_sols.append(bi_sols)
 
-        if (len(b0_sols) == 0) or (len(b1_sols) == 0) or (len(b2_sols) == 0) or (len(b3_sols) == 0):
+        if (len(b_sols[0]) == 0) or (len(b_sols[1]) == 0) or (len(b_sols[2]) == 0) or (len(b_sols[3]) == 0):
             continue
         else:
-            for b0 in b0_sols:
-                for b1 in b1_sols:
-                    for b2 in b2_sols:
-                        for b3 in b3_sols:
+            for b0 in b_sols[0]:
+                for b1 in b_sols[1]:
+                    for b2 in b_sols[2]:
+                        for b3 in b_sols[3]:
                             keybytes = (b0, b1, b2, b3)
                             possibilities.append(keybytes)
     return possibilities
 
-keys_0_7_10_13 = solve(ct[0], ct_[0], [0, 7, 10, 13], [2, 3, 1, 1])
-keys_1_4_11_14 = solve(ct[0], ct_[0], [1, 4, 11, 14], [2, 3, 1, 1])
-keys_2_5_8_15 = solve(ct[0], ct_[0], [2, 5, 8, 15], [2, 3, 1, 1])
-keys_3_6_9_12 = solve(ct[0], ct_[0], [3, 6, 9, 12], [2, 3, 1, 1])
+if __name__ == '__main__':
+    keys_p0 = []
+    keys_p1 = []
+    keys_p2 = []
+    keys_p3 = []
+    for pair_num in range(4):
+        keys_0_7_10_13 = []
+        keys_1_4_11_14 = []
+        keys_2_5_8_15 = []
+        keys_3_6_9_12 = []
+        for position in range(16):
+            col_num = get_fault_column(position)
+            assert(col_num!=None)
+            factors = get_factors(col_num)
+            assert(factors!=None)
+            keys_0_7_10_13.extend(solve(ct[pair_num], ct_[pair_num], [0, 13, 10, 7], factors[0]))
+            keys_1_4_11_14.extend(solve(ct[pair_num], ct_[pair_num], [4, 1, 14, 11], factors[1]))
+            keys_2_5_8_15.extend(solve(ct[pair_num], ct_[pair_num], [8, 5, 2, 15], factors[2]))
+            keys_3_6_9_12.extend(solve(ct[pair_num], ct_[pair_num], [12, 9, 6, 3], factors[3]))
+        keys_p0.append(keys_0_7_10_13)
+        keys_p1.append(keys_1_4_11_14)
+        keys_p2.append(keys_2_5_8_15)
+        keys_p3.append(keys_3_6_9_12)
+    final_set_0 = list(set(keys_p0[0]) & set(keys_p0[1]) & set(keys_p0[2]) & set(keys_p0[3]))
+    final_set_1 = list(set(keys_p1[0]) & set(keys_p1[1]) & set(keys_p1[2]) & set(keys_p1[3]))
+    final_set_2 = list(set(keys_p2[0]) & set(keys_p2[1]) & set(keys_p2[2]) & set(keys_p2[3]))
+    final_set_3 = list(set(keys_p3[0]) & set(keys_p3[1]) & set(keys_p3[2]) & set(keys_p3[3]))
+    print(len(final_set_0))
+    print(len(final_set_1))
+    print(len(final_set_2))
+    print(len(final_set_3))
+    # keys_1_4_11_14 = solve(ct[0], ct_[0], [1, 4, 11, 14], [2, 3, 1, 1])
+    # keys_2_5_8_15 = solve(ct[0], ct_[0], [2, 5, 8, 15], [2, 3, 1, 1])
+    # keys_3_6_9_12 = solve(ct[0], ct_[0], [3, 6, 9, 12], [2, 3, 1, 1])
 
-# keys = []
-# for p0 in keys_0_7_10_13:
-#     for p1 in keys_1_4_11_14:
-#         for p2 in keys_2_5_8_15:
-#             for p3 in keys_3_6_9_12:
-#                 key = (p0[0], p1[0], p2[0], p3[0],
-#                        p1[1], p2[1], p3[1], p0[1],
-#                        p2[2], p3[2], p0[2], p1[2],
-#                        p3[3], p0[3], p1[3], p2[3]
-#                       )
-#                 keys.append(key)
+    # keys = []
+    # for p0 in keys_0_7_10_13:
+    #     for p1 in keys_1_4_11_14:
+    #         for p2 in keys_2_5_8_15:
+    #             for p3 in keys_3_6_9_12:
+    #                 key = (p0[0], p1[0], p2[0], p3[0],
+    #                        p1[1], p2[1], p3[1], p0[1],
+    #                        p2[2], p3[2], p0[2], p1[2],
+    #                        p3[3], p0[3], p1[3], p2[3]
+    #                       )
+    #                 keys.append(key)
+
+    # keys_0_7_10_13 = solve(ct[1], ct_[1], [0, 7, 10, 13], [2, 3, 1, 1])
+    # keys_1_4_11_14 = solve(ct[1], ct_[1], [1, 4, 11, 14], [2, 3, 1, 1])
+    # keys_2_5_8_15 = solve(ct[1], ct_[1], [2, 5, 8, 15], [2, 3, 1, 1])
+    # keys_3_6_9_12 = solve(ct[1], ct_[1], [3, 6, 9, 12], [2, 3, 1, 1])
+
+    # keys = []
+    # for p0 in keys_0_7_10_13:
+    #     for p1 in keys_1_4_11_14:
+    #         for p2 in keys_2_5_8_15:
+    #             for p3 in keys_3_6_9_12:
+    #                 key = (p0[0], p1[0], p2[0], p3[0],
+    #                        p1[1], p2[1], p3[1], p0[1],
+    #                        p2[2], p3[2], p0[2], p1[2],
+    #                        p3[3], p0[3], p1[3], p2[3]
+    #                       )
+    #                 keys.append(key)
